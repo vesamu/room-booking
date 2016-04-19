32ee10eaf3a6e114c6f1654199105f61ecf5f4ba
Vesa <vesa.j.mujunen@gmail.com>
2016-04-19 09:31:47 +0300

    Add datepicker

--- booking.js
+++ booking.js
@@ -1,18 +1,22 @@
 $(function(){
-    
-    //Get rooms data from file
-    $.ajax({
-        type: "GET",
-        dataType: "json",
-        url: "rooms.json",
-        success: function(rooms){
-            $.each(rooms, function(i, room){
-                $("#rooms").append("<tr><td>" + room.name + "</td>" + "<td>" + room.location + "</td>" + "<td>" + room.persons + "</td></tr>");    
-            });
-        },
-        error: function(){
-            alert("Error on reading rooms data.");
-        }              
-    });
+  
+  $( "#datepicker" ).datepicker({
+    firstDay: 1
+  });
+      
+  //Get rooms data from file
+  $.ajax({
+    type: "GET",
+      dataType: "json",
+      url: "rooms.json",
+      success: function(rooms){
+        $.each(rooms, function(i, room){
+          $("#rooms").append("<td><ul><li>" + room.name + "</li>" + "<li>" + room.location + "</li>" + "<li>" + room.persons + "</li></ul></td>");    
+        });
+      },
+      error: function(){
+        alert("Error on reading rooms data.");
+      }              
+  });
    
 });
